b7ff55c874bed4fa5fb4f5ab55b7449d678fcc4b
Fox_Liz <uralova.elizaveta@lit1533.ru>
2026-03-27 09:02:35 +0300

    New classes

--- Projects/C#/Animal/obj/Debug/net8.0/Animal.AssemblyInfo.cs
+++ Projects/C#/Animal/obj/Debug/net8.0/Animal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Animal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e9fe5b8defd494694f7b456fb2121fbf5f6444")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21503cc18cc48bb96bd9b32293842cfb0955379")]
 [assembly: System.Reflection.AssemblyProductAttribute("Animal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Animal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
